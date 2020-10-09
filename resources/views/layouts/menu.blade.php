4b89994b966092759865d432f2d505163c452390
masriant <info.masrianto@gmail.com>
2020-10-09 18:38:42 +0700

    view laravel

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,27 @@
+<div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link" href="/about">About</a>
+          <a class="nav-link" href="/contact">Contact</a>
+          <a class="nav-link" href="/profile">Profile</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</div>
+
+
+<!-- <ul>
+  <li><a href="/">Home</a> </li>
+  <li><a href="/about">About</a></li>
+  <li><a href="/contact">Contact</a></li>
+  <li><a href="/profile">Profile</a></li>
+</ul> -->
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,4 @@
+@extends('app')
+@section('content')
+<h1>Profile</h1>
+@endsection
